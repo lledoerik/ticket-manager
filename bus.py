cd349e8cedb03f5641c15902ecd89d4a254b696a
Damià Brea <bc.damia@gmail.com>
2025-09-17 13:25:27 +0200

    Bus Getters & Setters

--- bus.py
+++ bus.py
@@ -1,7 +1,20 @@
 class Bus:
     def __init__(self, total_seats):
         self.__total_seats = total_seats
-        self.__taken_seats = total_seats
+        self.__taken_seats = 0
+
+    def getTotalSeats(self):
+        return self.__total_seats
+    
+    def setTotalSeats(self, total_seats):
+        self.__total_seats = total_seats
+
+    def getTakenSeats(self):
+        return self.__taken_seats
+    
+    def setTakenSeats(self, taken_seats):
+        self.__taken_seats = taken_seats
 
     def estado_venta(self):
-        return f"Asientos totales: {self.__total_seats}, Asientos ocupados: {self.__taken_seats}, Asientos disponibles: {self.__total_seats - self.__taken_seats}"
+        return f"Asientos totales: {self.getTotalSeats()}, Asientos ocupados: {self.getTakenSeats()}, Asientos disponibles: {self.getTotalSeats() - self.getTakenSeats()}"
+    
