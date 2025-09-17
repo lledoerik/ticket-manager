4f401609636431a1da50afaf5e75ff8bbddb6068
Damià Brea <bc.damia@gmail.com>
2025-09-17 13:20:14 +0200

    error in class Bus

--- bus.py
+++ bus.py
@@ -1,7 +1,7 @@
 class Bus:
-    def __init__(self, total_seats, taken_seats):
+    def __init__(self, total_seats):
         self.__total_seats = total_seats
-        self.__taken_seats = taken_seats
+        self.__taken_seats = total_seats
 
     def estado_venta(self):
         return f"Asientos totales: {self.__total_seats}, Asientos ocupados: {self.__taken_seats}, Asientos disponibles: {self.__total_seats - self.__taken_seats}"
